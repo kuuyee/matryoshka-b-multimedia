214182e11c5614b04fd03068a40277a118e42c86
eternal-flame-AD <ef@eternalflame.info>
2019-04-20 10:34:31 +0800

    #dev-311 configuration

--- conf/conf.go
+++ conf/conf.go
@@ -1,7 +1,10 @@
 package conf
 
 import (
+	"errors"
 	"io/ioutil"
+
+	"github.com/nfnt/resize"
 
 	"github.com/go-yaml/yaml"
 )
@@ -9,9 +12,47 @@ import (
 var conf *C
 
 type C struct {
+	API struct {
+		Listen string `yaml:"listen"`
+	} `yaml:"api"`
 	Storage struct {
 		Mode string `yaml:"mode"`
+		Path string `yaml:"path"`
 	} `yaml:"storage"`
+	Handlers struct {
+		Image struct {
+			MaxSize int64      `yaml:"maxsize"`
+			Resize  ResizeFunc `yaml:"resize"`
+		} `yaml:"image"`
+	} `yaml:"handlers"`
+}
+
+type ResizeFunc resize.InterpolationFunction
+
+func (r *ResizeFunc) UnmarshalYAML(origUnmarshal func(interface{}) error) error {
+	s := ""
+	if err := origUnmarshal(&s); err != nil {
+		return err
+	}
+	switch s {
+	case "", "bicubic":
+		*r = ResizeFunc(resize.Bicubic)
+	case "bilinear":
+		*r = ResizeFunc(resize.Bilinear)
+	case "lanczos2":
+		*r = ResizeFunc(resize.Lanczos2)
+	case "lanxzos3":
+		*r = ResizeFunc(resize.Lanczos3)
+	case "nearestneighbor":
+		*r = ResizeFunc(resize.NearestNeighbor)
+	default:
+		return errors.New("unrecognized resize interpolation function")
+	}
+	return nil
+}
+
+func GetParsed() C {
+	return *conf
 }
 
 func Get(fn string) C {
--- server.go
+++ server.go
@@ -1,25 +1,42 @@
 package main
 
 import (
+	"flag"
+
+	"github.com/nfnt/resize"
+
 	"github.com/kuuyee/matryoshka-b-multimedia/api"
+	"github.com/kuuyee/matryoshka-b-multimedia/conf"
 	"github.com/kuuyee/matryoshka-b-multimedia/internal/handlers"
 	"github.com/kuuyee/matryoshka-b-multimedia/internal/storage"
 	"github.com/kuuyee/matryoshka-b-multimedia/router"
-	"github.com/nfnt/resize"
 )
 
 func main() {
-	storage, err := storage.NewDiskStorage("data/")
-	if err != nil {
-		panic(err)
+	confPath := flag.String("c", "multimedia.yml", "config file")
+	flag.Parse()
+
+	serverConf := conf.Get(*confPath)
+
+	var storageH storage.S
+	switch serverConf.Storage.Mode {
+	case "disk":
+		var err error
+		storageH, err = storage.NewDiskStorage(serverConf.Storage.Path)
+		if err != nil {
+			panic(err)
+		}
+	default:
+		panic("unknown storage type")
 	}
+
 	api := api.NewAPI()
 	api.RegisterServiceHandler("image", &handlers.ImageHandler{
-		Storage:    storage,
-		MaxSize:    16 << 20,
-		ResizeAlgo: resize.Bicubic,
+		Storage:    storageH,
+		MaxSize:    serverConf.Handlers.Image.MaxSize,
+		ResizeAlgo: resize.InterpolationFunction(serverConf.Handlers.Image.Resize),
 		KeyedMutex: handlers.NewKeyedRWMutex(),
 	})
 	eng := router.New(api)
-	eng.Run(":8080")
+	eng.Run(serverConf.API.Listen)
 }
